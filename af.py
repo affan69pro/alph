3cdefe03ddcbe9d455bc002734877d44a5b99864
affan50pro <sweet.baby12325@gmail.com>
2024-10-30 17:59:53 +0400

    first commit

---
+++ af.py
@@ -0,0 +1,5 @@
+name=(input("↱enter a randon letter or number↰:"))
+if name==0 or 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9:
+    print("you entered a number😎")
+else:
+    print("you entered a letter😎")
